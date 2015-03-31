8fef22e76723c47d8f5b32d41a3e02f428953c13
jklingen <jens@getgreenshot.org>
2015-03-31 22:41:49 +0200

    removed println statement

--- BlogSpec.groovy
+++ BlogSpec.groovy
@@ -6,33 +6,32 @@ class FaqCheck extends spock.lang.Specification {
 
 	def static JEKYLL_BASE = "http://localhost:4000"
 
-	@Shared jekyllBlogUrls = extractBlogUrlsFromSitemap("$JEKYLL_BASE/sitemap.xml") 
+	@Shared jekyllBlogUrls = extractBlogUrlsFromSitemap("$JEKYLL_BASE/sitemap.xml")
 	@Shared wpBlogUrls = extractBlogUrlsFromSitemap("sitemap.xml")
 
 
 	def "all blog URLs from WP site should be available on local Jekyll"() {
-			
+
 		expect: "URL to be loaded successfully"
 			new URL(JEKYLL_BASE + blogUrl).getText()
-			
+
 		where:
 			blogUrl << wpBlogUrls
-				
+
 	}
-	
+
 	def "all blog URLs from WP site should be included in Jekylls sitemap"() {
-		
+
 		expect: "WP blog URL to be in Jekyll sitemap"
-		println (jekyllBlogUrls)
 			jekyllBlogUrls.contains(wpBlogUrl)
-			
+
 		where:
 			wpBlogUrl << wpBlogUrls
-			
+
 	}
-	
+
 	def extractBlogUrlsFromSitemap(String sitemapFile) {
-	
+
 		new XmlSlurper()
 				.parse(sitemapFile)
 				.url.loc
